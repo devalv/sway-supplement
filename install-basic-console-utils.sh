dec66f736f799d22e31bb35cddddf2c7637c7a79
Aleksei Devyatkin <63240599+devalv@users.noreply.github.com>
2026-07-28 21:32:59 +0300

    Install herdr after setting up fonts and starship
    Added installation of herdr after starship installation.

--- install-basic-console-utils.sh
+++ install-basic-console-utils.sh
@@ -8,12 +8,9 @@ echo "Basic console utils installed successfully!"
 # install additional fonts for starship
 wget -q --show-progress https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/FiraCode.zip
 unzip -q FiraCode.zip -d FiraCode
-
 FONT_DIR="$HOME/.local/share/fonts"
 mkdir -p "$FONT_DIR"
-
 find FiraCode -type f \( -name "*.ttf" -o -name "*.otf" \) -exec cp -v {} "$FONT_DIR/" \;
-
 fc-cache -f -v
 
 # remove downloaded files
@@ -21,3 +18,6 @@ rm -rf FiraCode FiraCode.zip
 
 # install starship and default firacode
 sudo apt-get install fonts-firacode starship -y -q || exit 1
+
+# install herdr
+curl -fsSL https://herdr.dev/install.sh | sh
