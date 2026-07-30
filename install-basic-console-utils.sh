37452dd999ff435f20dffae5ef20366d33efd5ba
Aleksei Devyatkin <63240599+devalv@users.noreply.github.com>
2026-07-30 23:20:11 +0300

    Replace 'mc' with 'ranger' in installation script

--- install-basic-console-utils.sh
+++ install-basic-console-utils.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sudo apt-get install git wget curl zsh duf bat zoxide eza mc -y -q || exit 1
+sudo apt-get install git wget curl zsh duf bat zoxide eza ranger -y -q || exit 1
 echo "Basic console utils installed successfully!"
 
 # install additional fonts for starship
